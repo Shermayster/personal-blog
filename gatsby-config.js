d84c9952dd9afebd020cb035b266f73788a9c735
Pavel Shermayster <sherm84@gmail.com>
2020-04-15 14:06:16 +0300

    aded description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Pavel Shermayster Personal Blog`,
+    author: `Pavel Shermayster`,
+    description: `my personal blog. I'm writing about web development, productivity, and `,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `ShermPavel`,
     },
   },
   plugins: [
