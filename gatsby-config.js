ef7c9d3b238437e9c23cb669984d6789a0ca6e4c
Pavel Shermayster <pavel.sh@openweb.com>
2021-12-24 15:57:46 +0200

    updated gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-styled-components`,
     "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-feed-mdx`,
@@ -64,6 +63,21 @@ module.exports = {
             },
           ],
         ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: "webp", // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
       },
     },
   ],
--- src/components/bio.js
+++ src/components/bio.js
@@ -34,13 +34,13 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpeg/" }) {
-      childImageSharp {
-        fixed(width: 50, height: 50) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
+    # avatar: file(absolutePath: { regex: "/profile-pic.jpeg/" }) {
+    #   childImageSharp {
+    #     fixed(width: 50, height: 50) {
+    #       ...GatsbyImageSharpFixed
+    #     }
+    #   }
+    # }
     site {
       siteMetadata {
         author
